653c2816992c14a924359ca35937f2c7ab8e8c89
sdurand63 <sarahdurand2007@gmail.com>
2026-02-06 14:13:13 +0100

    offer details page html

---
+++ details-offres.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ThePiston</title>
+</head>
+
+<body>
+    
+    <main>
+        <header>
+            <h1>Titre de l'offre</h1>
+            <p>Nom de l'entreprise</p>
+        </header>
+
+        <div>
+            <button>Postuler</button>
+        </div>
+
+        <div>
+            <h2>Compétences requises</h2>
+            <ul>
+                <li>Compétence 1</li>
+                <li>Compétence 2</li>
+                <li>Compétence 3</li>
+                <li>Compétence 4</li>
+            </ul>
+        </div>
+
+        <section>
+            <h2>Description de l'offre</h2>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel sapien eget nunc efficitur bibendum. Sed at ligula a nunc efficitur bibendum. Sed at ligula a nunc efficitur bibendum.</p>
+            </div>
+        </section>
+
+        <div>
+            <h2>Rémunération</h2>
+            <p>1euro/mois</p>
+        </div>
+              
+
+
+    </main>
+
+</body>
+</html>
