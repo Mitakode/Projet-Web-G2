6a8273475de0e47c4b3ba6d3d9c858fa1b3e94b0
sdurand63 <sarahdurand2007@gmail.com>
2026-02-06 15:53:54 +0100

    css body

--- accueil.php
+++ accueil.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="images/icon.png">
+    <link rel="stylesheet" href="style.css">
     <title>Accueil - ThePiston</title>
 </head>
 
@@ -12,69 +13,78 @@
     <?php include 'header.php'; ?>
     
 <main>
-    <section>
+    <section class="search-section">
         <h1>Les offres disponibles</h1>
 
-        <form action="offres.html" method="GET">
-            <input type="text" name="recherche" placeholder="Rechercher...">
-            <input type="submit" value="🔎">
-            <br>
-            <select name="competences">
-                <option value="">Mes compétences</option>
-                <option value="competence1">Compétence 1</option>
-                <option value="competence2">Compétence 2</option>
-                <option value="competence3">Compétence 3</option>
-            </select>
+        <form class="search-form"  action="offres.php" method="GET">
+        <div class="search-bar-wrapper">
+            <input class="search-input" type="text" name="recherche" placeholder="Rechercher...">
+            <input type="submit" class="search-btn" value="🔎">
+        </div>
 
-            <select name="type_offre">
-                <option value="">Type d'offre</option>
-                <option value="Stage">Stage</option>
-                <option value="Alternance">Alternance</option>
-            </select>
+            <div class="filters-container">
+                <select class="filter-item" name="competences">
+                    <option value="">Mes compétences</option>
+                    <option value="competence1">Compétence 1</option>
+                    <option value="competence2">Compétence 2</option>
+                    <option value="competence3">Compétence 3</option>
+                </select>
 
-            <input type="text" name="localisation" placeholder="Localisation...">
+                <select class="filter-item" name="type">
+                    <option value="">Type d'offre</option>
+                    <option value="stage">Stage</option>
+                    <option value="Alternance">Alternance</option>
+                </select>
 
-            <input type="text" name="entreprise"  placeholder="Entreprise...">
+                <input type="text" class="filter-item" name="loc" placeholder="Localisation...">
+                
+                <input type="text" class="filter-item" name="ent" placeholder="Entreprise...">
+            </div>
+           
         </form>
     </section>
 
-    <section>
-        <h2>Qui sommes-nous ?</h2>
-        <p>Nous sommes nous mêmes 4 étudiants pour qui trouver un stage fut laborieux, très laborieux. Le monde du travail est une bêtre sauvage pour les étudiants. C'est pourquoi nous avons eu l'idée de créer ThePiston, le site regroupant un pannel d'offres de stage et de contrat d'apprentissage pour aider nos compagnons étudiants de  la France entière. </p>
-    </section>
+    
 
-    <section>
-        <h2>Nos statistiques</h2>
-        <div>
+    <div class="info-grid">
+        <section class="info-box">
+            <h2>Qui sommes-nous ?</h2>
+            <p>Nous sommes nous mêmes 4 étudiants pour qui trouver un stage fut laborieux, très laborieux. Le monde du travail est une bêtre sauvage pour les étudiants. C'est pourquoi nous avons eu l'idée de créer ThePiston, le site regroupant un pannel d'offres de stage et de contrat d'apprentissage pour aider nos compagnons étudiants de  la France entière. </p>
+        </section>
+
+        <section class="info-box">
+            <h2>Nos statistiques</h2>
             <div>
                 <div>
-                    <h3>Statistiques générales</h3>
-                    <ul>
-                        <li>Nombre d'étudiants inscrits : </li>
-                        <li>Nombre d'offres disponibles : </li>
-                        <li>Nombre moyen de canidature par offre : </li>
-                    </ul>
-                </div>
+                    <div>
+                        <h3>Statistiques générales</h3>
+                        <ul>
+                            <li>Nombre d'étudiants inscrits : </li>
+                            <li>Nombre d'offres disponibles : </li>
+                            <li>Nombre moyen de canidature par offre : </li>
+                        </ul>
+                    </div>
 
-                <div>
-                    <h3>Top des offres</h3>
-                    <ul>
-                        <li>Offre 1 : Nombre de candidatures</li>
-                        <li>Offre 2 : Nombre de candidatures</li>
-                        <li>Offre 3 : Nombre de candidatures</li>
-                    </ul>
-                </div>
+                    <div>
+                        <h3>Top des offres</h3>
+                        <ul>
+                            <li>Offre 1 : Nombre de candidatures</li>
+                            <li>Offre 2 : Nombre de candidatures</li>
+                            <li>Offre 3 : Nombre de candidatures</li>
+                        </ul>
+                    </div>
 
-                <div>
-                    <h3>Répartition des offres par rapport à leur durée</h3>
-                    <div>1-3</div>
-                    <div>4-6</div>
-                    <div>6+</div>
-                </div>
+                    <div>
+                        <h3>Répartition des offres par rapport à leur durée</h3>
+                        <div>1-3</div>
+                        <div>4-6</div>
+                        <div>6+</div>
+                    </div>
 
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </main>
 
     <?php include 'footer.php'; ?>
