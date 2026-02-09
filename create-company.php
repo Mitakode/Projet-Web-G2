a4d1778c928cd5e851350bffa047486ec062c4d2
sdurand63 <sarahdurand2007@gmail.com>
2026-02-09 10:25:39 +0100

    CSS form create company

--- create-company.php
+++ create-company.php
@@ -4,28 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="images/icon.png">
+    <link rel="stylesheet" href="style.css">
     <title>Créer une entreprise - ThePiston</title>
 </head>
 <body>
     <?php include 'header.php'; ?>
 
-    <form>
-        <h2>Créer une entreprise</h2>
-
-        <label>Nom de l'entreprise</label>
-        <input type="text">
-
-        <label>Email et téléphone de contact</label>
-        <input type="text">
+    <section class="frame-form">
+        <h1>Créer une entreprise</h1>
         
-        <label>Description</label>
-        <input type="text">
-        
-        <br><br>
-        <button type="submit">
-            Créer une entreprise
-        </button>
-    </form>
+        <form class="form-test" action="/accueil" method="POST">
+
+            <label>Nom de l'entreprise</label>
+            <input class="form-input" type="text">
+
+            <label>Contact (Email et téléphone)</label>
+            <input class="form-input" type="text">
+
+            <label>Description</label>
+            <textarea class="form-input" rows="10"></textarea>
+            
+            <br><br>
+            <button class="form-button" type="submit">
+                Créer une entreprise
+            </button>
+        </form>
+    </section>
 
     <?php include 'footer.php'; ?>
 </body>
