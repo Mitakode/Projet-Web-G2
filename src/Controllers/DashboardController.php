bbffc41f33ae4ef33aeb27298e8bbf037cd94fa5
sdurand63 <sarahdurand2007@gmail.com>
2026-03-21 18:42:18 +0100

    fixing searching students and style, addding pagination for the student list and admin/pilot differences

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -18,7 +18,19 @@ class DashboardController{
         $promotion = $_GET['promotion'] ?? '';
 
         $students = $this->model->searchStudents($surname, $name, $promotion);
-        echo $this->twig->render('admin.html.twig', ['etudiants' => $students]);
+
+        // Gérer la pagination
+        $paginator = new Paginator($students, 5);
+        
+        // Envoyer le tout à la vue Twig
+        echo $this->twig->render('DashboardAdmin.html.twig', [
+            'etudiants' => $paginator->getCurrentPageItems(),
+            'total_pages'      => $paginator->getTotalPages(),
+            'current_page'     => $_GET['page'] ?? 1,
+            'surname'      => $surname,
+            'name'             => $name,
+            'promotion'        => $promotion
+        ]);
     }
 
 }
