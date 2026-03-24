863fece1552805f57cd8dd8f0100baf05549a828
Maxence <alaouamaxence@gmail.com>
2026-03-24 10:18:08 +0100

    Deleted redirectToDashboardByRole

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -18,7 +18,8 @@ class AuthController
         $error = null;
 
         if (!empty($_SESSION)) {
-            $this->redirectToDashboardByRole($_SESSION['user_role']);
+            header('Location: /dashboard');
+            exit;
         }
 
         // on traite le résultat du form de connexion
@@ -46,7 +47,8 @@ class AuthController
                 $_SESSION['user_nom']  = $user['Nom'];
                 $_SESSION['user_prenom'] = $user['Prenom'];
 
-                $this->redirectToDashboardByRole($role);
+                header('Location: /dashboard');
+                exit;
             }
 
             $error = 'Email ou mot de passe incorrect.';
@@ -98,26 +100,6 @@ class AuthController
             header('Location: /login');
             exit;
         }
-    }
-
-    private function redirectToDashboardByRole(string $role): void
-    {
-        switch ($role) {
-            case 'admin':
-                header('Location: /dashboard/admin');
-                break;
-            case 'pilote':
-                header('Location: /dashboard/admin');
-                break;
-            case 'etudiant':
-                header('Location: /dashboard/student');
-                break;
-            default:
-                header('Location: /login');
-                break;
-        }
-
-        exit;
     }
 
     private function verifyPassword(array $user, string $plainPassword): bool
