0354673adc28f923eb2c4232059c198f49342433
Maxence <alaouamaxence@gmail.com>
2026-03-27 10:45:22 +0100

    Removed carousel auto slide

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -6,9 +6,7 @@
 (function () {
     // Utilisation de la directive "use strict" pour activer le mode strict en JavaScript
     // Cela implique une meilleure gestion des erreurs et une syntaxe plus stricte pour le code
-    "use stict"
-    // Déclare la constante pour la durée de chaque slide
-    const slideTimeout = 5000;
+    "use strict";
     // Récupère les boutons de navigation
     const prev = document.querySelector('#prev');
     const next = document.querySelector('#next');
@@ -16,8 +14,6 @@
     const $slides = document.querySelectorAll('.slide');
     // Initialisation de la variable pour les "dots"
     let $dots;
-    // Initialisation de la variable pour l'intervalle d'affichage des slides
-    let intervalId;
     // Initialisation du slide courant à 1
     let currentSlide = 1;
     // Fonction pour afficher un slide spécifique en utilisant un index
@@ -28,11 +24,6 @@
         $slides.forEach($elt => $elt.style.transform = `translateX(-${currentSlide * 100}%)`);
         // Boucle sur tous les "dots" pour mettre à jour la couleur par la classe "active" ou "inactive"
         $dots.forEach(($elt, key) => $elt.classList = `dot ${key === currentSlide? 'active': 'inactive'}`);
-    }
-    // Fonction pour afficher le prochain slide
-    function showSlide() {
-        slideTo(currentSlide);
-        currentSlide++;
     }
     // Boucle pour créer les "dots" en fonction du nombre de slides
     for (let i = 1; i <= $slides.length; i++) {
@@ -48,20 +39,10 @@
     prev.addEventListener('click', () => slideTo(--currentSlide))
     // Ajout d'un écouteur d'événement "click" sur le bouton "next" pour afficher le slide suivant
     next.addEventListener('click', () => slideTo(++currentSlide))
-    // Initialisation de l'intervalle pour afficher les slides
-    intervalId = setInterval(showSlide, slideTimeout)
-    // Boucle sur tous les éléments de type "slide" pour ajouter des écouteurs d'événement pour les interactions avec la souris et le toucher
+    // Boucle sur tous les éléments de type "slide" pour ajouter des écouteurs d'événement toucher
     $slides.forEach($elt => {
         let startX;
         let endX;
-        // Efface l'intervalle d'affichage des slides lorsque la souris passe sur un slide
-        $elt.addEventListener('mouseover', () => {
-            clearInterval(intervalId);
-        }, false)
-        // Réinitialise l'intervalle d'affichage des slides lorsque la souris sort d'un slide
-        $elt.addEventListener('mouseout', () => {
-            intervalId = setInterval(showSlide, slideTimeout);
-        }, false);
         // Enregistre la position initiale du toucher lorsque l'utilisateur touche un slide
         $elt.addEventListener('touchstart', (event) => {
             startX = event.touches[0].clientX;
